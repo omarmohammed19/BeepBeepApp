a22813734e2727aafd66c3bde7f65ea83be2b3d8
Shama352 <65794387+Shama352@users.noreply.github.com>
2021-11-23 20:20:18 +0200

    Driver Class

--- src/User.java
+++ src/User.java
@@ -1,10 +1,8 @@
 public class User {
-
         public String username;
         public String email;
         public String password;
         public String phone;
         public String Type;
-
 }
 
