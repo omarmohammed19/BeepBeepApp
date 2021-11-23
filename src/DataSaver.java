85f9c6d8d22b24feff4c4d091f1ae19cbe1f5e0d
adhamahmed4 <adhamahmeds2312@gmail.com>
2021-11-23 20:27:03 +0200

    DataSaver Class

---
+++ src/DataSaver.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataSaver {
+    List<Passenger> passengers= new ArrayList<>();
+    List<Driver> drivers= new ArrayList<>();
+    List<List<String>> rides= new ArrayList<List<String>>();
+}
+
