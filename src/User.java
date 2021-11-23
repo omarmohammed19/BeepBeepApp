5b7d128cc2def86adef2582498b35822c4a59aa5
omarmohammed19 <75382998+omarmohammed19@users.noreply.github.com>
2021-11-23 19:35:00 +0200

    User Class

---
+++ src/User.java
@@ -0,0 +1,10 @@
+public class User {
+
+        public String username;
+        public String email;
+        public String password;
+        public String phone;
+        public String Type;
+
+}
+
