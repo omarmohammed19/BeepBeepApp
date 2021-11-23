c9f47e72a825bf5f96d1958f59e6486c304287ed
Shama352 <65794387+Shama352@users.noreply.github.com>
2021-11-23 20:31:59 +0200

    Ride Class

---
+++ src/Ride.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ride {
+    String source;
+    String destination;
+    List<Integer> Offers = new ArrayList<Integer>();
+    public Ride() {
+
+    }
+}
