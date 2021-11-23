4a5a798b3438b7d7ad4e4fb7690d6a0f3f016884
omarmohammed19 <75382998+omarmohammed19@users.noreply.github.com>
2021-11-23 19:39:50 +0200

    Passenger Class

---
+++ src/Passenger.java
@@ -0,0 +1,27 @@
+public class Passenger extends User {
+    public Passenger(String Type ,String user, String mail, String pass, String mobile)
+    {
+        this.Type="p";
+        this.username=user;
+        this.password=pass;
+        this.email=mail;
+        this.phone=mobile;
+    }
+    public boolean login(String loginname, String loginpass) {
+        //for (int i = 0; i < passengers.size(); i++) {
+        if (loginname.equals(username)) {
+            if (loginpass.equals(password)) {
+                System.out.println("Passenger Login Successful");
+                return true;
+            } else {
+                System.out.println("Password Incorrect Login failed");
+                return false;
+            }
+        }
+        else
+        {
+            System.out.println("Username Incorrect Login failed");
+            return false;
+        }
+    }
+}
