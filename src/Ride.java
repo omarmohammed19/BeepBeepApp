cc17cf790c8e51fd8f7a5b5a049bd3531e6935d9
Shama352 <65794387+Shama352@users.noreply.github.com>
2021-11-23 20:33:26 +0200

    Ride Class

--- src/Ride.java
+++ src/Ride.java
@@ -8,4 +8,20 @@ public class Ride {
     public Ride() {
 
     }
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
 }
